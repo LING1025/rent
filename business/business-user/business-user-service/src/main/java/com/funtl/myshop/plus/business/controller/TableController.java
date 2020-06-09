c3c97e9527475a058c14a076f706bfee73efde9f
justone <2274104495@qq.com>
2020-06-09 17:32:36 +0800

    2020-06-09 third

--- business/business-user/business-user-service/src/main/java/com/funtl/myshop/plus/business/controller/TableController.java
+++ business/business-user/business-user-service/src/main/java/com/funtl/myshop/plus/business/controller/TableController.java
@@ -2,11 +2,8 @@ package com.funtl.myshop.plus.business.controller;
 
 import com.funtl.myshop.plus.commons.dto.ResponseResult;
 import com.funtl.myshop.plus.provider.api.PerformanceService;
-import com.funtl.myshop.plus.provider.api.ReportFormService;
 import com.funtl.myshop.plus.provider.domain.ReportForms;
 import com.funtl.myshop.plus.provider.dto.RptQueryParam;
-import com.funtl.myshop.plus.provider.dto.RptQueryParams;
-import com.google.common.collect.Lists;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
@@ -17,10 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * 报表信息管理
@@ -31,21 +25,7 @@ import java.util.Map;
 @RequestMapping(value = "table")
 public class TableController {
     @Reference(version = "1.0.0")
-    private ReportFormService reportFormService;
-
-    @Reference(version = "1.0.0")
     private PerformanceService performanceService;
-
-    @ApiOperation(value = "获取营业报表信息")
-    @GetMapping(value = "queryRptQ")
-    public ResponseResult<List<ReportForms>> queryRptQ(@RequestParam(name = "type",required = false) Integer type,
-                                                       @RequestParam(name = "userID",required = false) Long userID,
-                                                       @RequestParam(name = "startDT_F",required = false) String startDT_F,
-                                                       @RequestParam(name = "startDT_E",required = false) String startDT_E){
-        RptQueryParams rptQueryParams = new RptQueryParams(type,userID,startDT_F,startDT_E);
-        List<ReportForms> list = reportFormService.getRptQ(rptQueryParams);
-        return new ResponseResult<>(ResponseResult.CodeStatus.OK,"查询成功",list);
-    }
 
     @ApiOperation(value = "获取营业报表信息")
     @ApiImplicitParams({
