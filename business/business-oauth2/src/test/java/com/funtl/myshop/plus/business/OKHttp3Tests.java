1715048391a8128fbd68f053b1ecacf34a5b8822
LING1025 <2274104495@qq.com>
2020-08-10 16:31:31 +0800

    报表排序用1替换一

--- business/business-oauth2/src/test/java/com/funtl/myshop/plus/business/OKHttp3Tests.java
+++ business/business-oauth2/src/test/java/com/funtl/myshop/plus/business/OKHttp3Tests.java
@@ -21,20 +21,9 @@ public class OKHttp3Tests {
 
     @Test
     public void test() throws NoSuchAlgorithmException {
-        String Salt = "/QJI/OYzoWZAiclktXY/sA==";
-        System.out.println(Salt);
-        //定义一个密码，这里就不从数据库取了
-        String p = "123456";
+        String p = "!QAZ2wsx";
         //打印md5加密后的密码
-        System.out.println("md5加密结果："+DigestUtils.md5DigestAsHex(p.getBytes()));
-        //登录时，从页面获取密码与数据库密码进行比较，这里就不获取，直接定义
-        String password = DigestUtils.md5DigestAsHex(p.getBytes());//数据库保存的密码
-        String pwd = "123456";//从页面获取的密码1
-        String pwd2 = "111111";//从页面获取的密码2
-        //使用md5验证
-        System.out.println(DigestUtils.md5DigestAsHex(pwd.getBytes()).equals(password));
-        System.out.println(DigestUtils.md5DigestAsHex(pwd2.getBytes()).equals(password));
-
+        System.out.println(passwordEncoder.encode(p));
     }
 
     @Test
