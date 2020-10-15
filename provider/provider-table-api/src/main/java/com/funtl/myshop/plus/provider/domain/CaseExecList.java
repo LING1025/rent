333a4f12664a6593b534db92eb7d84978062171c
LING1025 <2274104495@qq.com>
2020-10-15 17:35:18 +0800

    案件维护汇出数据未完成

---
+++ provider/provider-table-api/src/main/java/com/funtl/myshop/plus/provider/domain/CaseExecList.java
@@ -0,0 +1,175 @@
+package com.funtl.myshop.plus.provider.domain;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Data
+@ApiModel(value = "案件维护汇出数据")
+public class CaseExecList implements Serializable {
+    @ApiModelProperty(value = "业务单位")
+    private String depName;
+
+    @ApiModelProperty(value = "业代")
+    private String fName;
+
+    @ApiModelProperty(value = "契约编号")
+    private String orderNo;
+
+    @ApiModelProperty(value = "承租人统编")
+    private Long tradeItemAuto;
+
+    @ApiModelProperty(value = "公司名称")
+    private String comFName;
+
+    @ApiModelProperty(value = "是否是公司")
+    private String idenType;
+
+    @ApiModelProperty(value = "所属公司")
+    private String sName;
+
+    @ApiModelProperty(value = "状态")
+    private String status;
+
+    @ApiModelProperty(value = "牌照号码")
+    private String makNo;
+
+    @ApiModelProperty(value = "车种")
+    private String category;
+
+    @ApiModelProperty(value = "客户来源")
+    private String custSource;
+
+    @ApiModelProperty(value = "租赁性质")
+    private String rentType;
+
+    @ApiModelProperty(value = "总厂牌")
+    private String factoryBrandName;
+
+    @ApiModelProperty(value = "厂牌")
+    private String brandName;
+
+    @ApiModelProperty(value = "车型")
+    private String clasenName;
+
+    @ApiModelProperty(value = "车色")
+    private String carColor;
+
+    @ApiModelProperty(value = "经销商")
+    private String sellerFName;
+
+    @ApiModelProperty(value = "每月租金")
+    private String mAmt;
+
+    @ApiModelProperty(value = "总租金=a.MAmt*a.MM ")
+    private String totalAmt;
+
+    @ApiModelProperty(value = "领照日")
+    private Date makDT;
+
+    @ApiModelProperty(value = "牌价")
+    private BigDecimal listPrice;
+
+    @ApiModelProperty(value = "进价")
+    private BigDecimal getPrice;
+
+    @ApiModelProperty(value = "折价")
+    private BigDecimal disPrice;
+
+    @ApiModelProperty(value = "折价率")
+    private BigDecimal disPriceTax;
+
+    @ApiModelProperty(value = "配件")
+    private BigDecimal accessary;
+
+    @ApiModelProperty(value = "佣金")
+    private BigDecimal pushMoney;
+
+    @ApiModelProperty(value = "残值")
+    private BigDecimal salvage;
+
+    @ApiModelProperty(value = "保证金")
+    private BigDecimal dptAmt;
+
+    @ApiModelProperty(value = "月维修费")
+    private BigDecimal carMtnAmt;
+
+    @ApiModelProperty(value = "贷款金额")
+    private BigDecimal loanAmount;
+
+    @ApiModelProperty(value = "起租日")
+    private Date StartDT;
+
+    @ApiModelProperty(value = "到期日")
+    private Date EndDT;
+
+    @ApiModelProperty(value = "期数")
+    private Integer mm;
+
+    @ApiModelProperty(value = "客户评等")
+    private String creditLevel;
+
+    @ApiModelProperty(value = "申购单号")
+    private Long orderAuto;
+
+    @ApiModelProperty(value = "授信单号")
+    private Long creditMainAuto;
+
+    @ApiModelProperty(value = "合约编号")
+    private String contractNo;
+
+    @ApiModelProperty(value = "交车日期")
+    private Date forDT;
+
+    @ApiModelProperty(value = "付款方式")
+    private String payType;
+
+    @ApiModelProperty(value = "车辆来源")
+    private String carSource;
+
+    @ApiModelProperty(value = "成交利率")
+    private BigDecimal rentRate;
+
+    @ApiModelProperty(value = "承作利率")
+    private BigDecimal finalRate;
+
+    @ApiModelProperty(value = "承作价格")
+    private BigDecimal rentAmt;
+
+    @ApiModelProperty(value = "实际利率")
+    private BigDecimal bonusRate;
+
+    @ApiModelProperty(value = "业务类别")
+    private String businessType;
+
+    @ApiModelProperty(value = "试算业务类别")
+    private String businessOrdersType;
+
+    @ApiModelProperty(value = "购置税实付金额")
+    private BigDecimal realAmt;
+
+    @ApiModelProperty(value = "配件实付金额")
+    private BigDecimal accRealAmt;
+
+    @ApiModelProperty(value = "业绩日")
+    private Date yjCDT;
+
+    @ApiModelProperty(value = "使用性质")
+    private String useType;
+
+    @ApiModelProperty(value = "是否一次性租赁")
+    private String onetime;
+
+    @ApiModelProperty(value = "专案")
+    private String projectName;
+
+    @ApiModelProperty(value = "货车")
+    private String trucks;
+
+    @ApiModelProperty(value = "公司型态")
+    private String comType;
+}
